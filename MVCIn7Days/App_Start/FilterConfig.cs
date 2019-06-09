90398a132f0544a9e78e4c80d6c1c6eb7f3ae7b5
saud ansari <saudblaze@gmail.com>
2019-06-09 11:00:57 +0530

    Add project files.

---
+++ MVCIn7Days/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIn7Days
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
